8e80a0bf2d358927b77cb7e7704c12ba52307c04
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-10-12 18:01:42 -0500

    --compile

--- src/main/java/glang/launcher/ScriptLauncher.java
+++ src/main/java/glang/launcher/ScriptLauncher.java
@@ -1,16 +1,17 @@
 package glang.launcher;
 
+import glang.compiler.bytecode.GlangCompiler;
 import glang.compiler.error.CompileFailedException;
 import glang.runtime.cl.GlangClassLoader;
+import org.objectweb.asm.ClassWriter;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 public class ScriptLauncher {
     public static void main(String[] args) throws Throwable {
@@ -18,12 +19,17 @@ public class ScriptLauncher {
         jarFile = jarFile.substring(jarFile.lastIndexOf('/') + 1);
 
         if (args.length == 0) {
-            System.err.println("Usage: java -jar " + jarFile + " <script> [args...]");
+            System.err.println("Usage: java -jar " + jarFile + " [--compile] <script> [args...]");
             System.exit(1);
             return;
         }
 
         final Deque<String> arguments = new ArrayDeque<>(List.of(args));
+
+        final boolean compileOnly = arguments.element().equals("--compile");
+        if (compileOnly) {
+            arguments.remove();
+        }
 
         final String scriptPathStr = arguments.removeFirst();
         if (!scriptPathStr.endsWith(".glang")) {
@@ -57,6 +63,31 @@ public class ScriptLauncher {
             .relativize(scriptPath)
             .toString()
             .replace(FileSystems.getDefault().getSeparator(), "/");
+
+        if (compileOnly) {
+            final Map<String, ClassWriter> result = new HashMap<>();
+            try {
+                final GlangCompiler compiler = new GlangCompiler(
+                    relativePath.substring(0, relativePath.length() - 6).replace('/', '.'),
+                    Files.readString(scriptPath),
+                    name -> result.computeIfAbsent(name, n -> new ClassWriter(ClassWriter.COMPUTE_FRAMES))
+                );
+                compiler.compile(relativePath.substring(relativePath.lastIndexOf('/') + 1));
+                result.forEach((clazz, writer) -> {
+                    try {
+                        Files.write(Path.of(clazz.replace('.', '/') + ".class"), writer.toByteArray());
+                    } catch (IOException e) {
+                        System.err.println("Failed to write " + clazz);
+                        //noinspection ThrowablePrintedToSystemOut
+                        System.err.println(e);
+                    }
+                });
+            } catch (CompileFailedException e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
+            return;
+        }
 
         final ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
         final var cl = new GlangClassLoader(new URL[] {cwd.toUri().toURL()});
